d6ece74478664e7665f0c245e6c4ed62c6e69afc
Freddy Shema <shemafre10@gmail.com>
2025-03-27 22:28:13 -0700

    Added an Internet Gateway, Enabled DNS hostnames & Created a Public route table

--- vpc.tf
+++ vpc.tf
@@ -3,19 +3,35 @@ resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
+  # Enable DNS hostnames and support
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Name = "main_vpc"
   }
 }
 
-# Subnets in Different Availability Zones
+# Internet Gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Main VPC Internet Gateway"
+  }
+}
+
+# Subnets
 resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
+  # Make subnet public
+  map_public_ip_on_launch = true
+
   tags = {
-    Name = "Subnet 1"
+    Name = "Public Subnet 1"
   }
 }
 
@@ -24,12 +40,40 @@ resource "aws_subnet" "subnet_2" {
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 
+  # Make subnet public
+  map_public_ip_on_launch = true
+
   tags = {
-    Name = "Subnet 2"
+    Name = "Public Subnet 2"
   }
 }
 
-# Update the Subnets Data Source
+# Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "subnet_1_association" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Update Subnets Data Source
 data "aws_subnets" "main_subnet" {
   filter {
     name   = "vpc-id"
