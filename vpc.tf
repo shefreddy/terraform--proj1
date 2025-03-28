d6b1ff1f9510d373a18183ce6b4ffc9df4e20a07
Freddy Shema <shemafre10@gmail.com>
2025-03-27 21:48:33 -0700

    Add different Availability Zones to vpc

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-#vpc module
+# VPC Resource
 resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -8,19 +8,33 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
-#subnet
-resource "aws_subnet" "main_subnet" {
-    vpc_id = aws_vpc.main_vpc.id
-    cidr_block = "10.0.1.0/24"
+# Subnets in Different Availability Zones
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-    tags = {
-      Name = "Main subnet"
-    }
+  tags = {
+    Name = "Subnet 1"
+  }
 }
 
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Subnet 2"
+  }
+}
+
+# Update the Subnets Data Source
 data "aws_subnets" "main_subnet" {
   filter {
     name   = "vpc-id"
     values = [aws_vpc.main_vpc.id]
   }
+
+  depends_on = [aws_subnet.subnet_1, aws_subnet.subnet_2]
 }
